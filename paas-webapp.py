e304c83d8784cab7af7faebd65f36136d47c407e
Vasantha Nithi <192424137.simats@saveetha.com>
2025-09-18 08:13:35 +0530

    Update paas-webapp.py for Azure PaaS demo

--- paas-webapp.py
+++ paas-webapp.py
@@ -1,15 +1,16 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-host = "0.0.0.0"
-port = 8000
-
-class MyHandler(SimpleHTTPRequestHandler):
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello, PaaS Demo on Azure without Flask!")
+        self.wfile.write(b"<h2>Hello, PaaS Demo on Azure without Flask!</h2>")
 
-httpd = HTTPServer((host, port), MyHandler)
-print(f"Server running on http://{host}:{port}")
-httpd.serve_forever()
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 8000))
+    server_address = ("0.0.0.0", port)
+    httpd = HTTPServer(server_address, MyHandler)
+    print(f"Server running on http://0.0.0.0:{port}")
+    httpd.serve_forever()
